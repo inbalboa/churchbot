2af76252bf780e21c6decbd747a679eaa558a1a8
inbalboa <inbalboa@gmail.com>
2019-04-21 22:47:10 +0300

    Fix settings

--- churchill_bot.py
+++ churchill_bot.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
 
-import argparse
-import configparser
 import logging
-import pathlib
+import os
 import sys
 import tweepy
 
 app_name = 'church_bot'
 
-def parse_args() -> tuple:
-    parser = argparse.ArgumentParser(description='Churchill twitter bot')
-    parser.add_argument('--config_file', type=str, help='Config file', default=f'{pathlib.Path.home()}/.config/{app_name}.cfg')
-    args = parser.parse_args()
-    return args
-
 def read_config(config_file: str) -> tuple:
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    
-    secret_section = config['SECRETS']
-    consumer_key = secret_section.get('consumer_key', None)
-    consumer_secret = secret_section.get('consumer_secret', None)
-    access_token_key = secret_section.get('access_token_key', None)
-    access_token_secret = secret_section.get('access_token_secret', None)
-    
-    files_section = config['FILES']
-    log_path = files_section.get('log_path', None)
-    state_file = files_section.get('state_file', f'{pathlib.Path.home()}/.local/share/{app_name}/{app_name}.state')
-    
-    text_section = config['TEXT']
-    search_query = text_section.get('search_query', None)
-    status_text = text_section.get('status_text', None)
+    consumer_key = os.environ.get('consumer_key', None)
+    consumer_secret = os.environ.get('consumer_secret', None)
+    access_token_key = os.environ.get('access_token_key', None)
+    access_token_secret = os.environ.get('access_token_secret', None)
+    search_query = os.environ.get('search_query', None)
+    status_text = os.environ.get('status_text', None)
     
     return consumer_key, consumer_secret, access_token_key, access_token_secret, log_path, state_file, search_query, status_text
 
@@ -50,33 +32,21 @@ def get_api(consumer_key: str, consumer_secret: str, access_token: str, access_t
     return tweepy.API(auth)
 
 def get_last_id(state_file: str) -> str:
-    result = None
-    try:
-        with open(state_file) as file:
-            array_state = [row.strip() for row in file]
-            if len(array_state):
-                result = array_state[0]
-    except FileNotFoundError:
-        pass
+    return os.environ.get('last_id', '1117855342552133637')
 
-    return result if result else '1114964897769623553'
+def set_last_id(tweet_id: str):
+    return os.environ.set(tweet_id)
 
-def set_last_id(state_file: str, tweet_id: str):
-    with open(state_file, 'w') as file:
-        file.write(tweet_id)
-        
 def check_phrase(phrase: str) -> bool:
     return True
 
 def main():
-    parsed_args = parse_args()
-
-    consumer_key, consumer_secret, access_token_key, access_token_secret, log_path, state_file, search_query, status_text = read_config(parsed_args.config_file)
+    consumer_key, consumer_secret, access_token_key, access_token_secret, log_path, state_file, search_query, status_text = read_config()
     
-    logger = get_logger(log_path)
+    logger = get_logger()
     api = get_api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-    tweet_id = get_last_id(state_file)
+    tweet_id = get_last_id()
     try:
         tweets = sorted(tweepy.Cursor(api.search, q=search_query, tweet_mode='extended', since_id=tweet_id).items(), key=lambda x: x.id_str)
     except (Exception, tweepy.TweepError) as error:
@@ -86,7 +56,7 @@ def main():
     for tweet in tweets:
         try:
             api.update_status(f'@{tweet.author.screen_name} {status_text}', in_reply_to_status_id=tweet.id_str)
-            set_last_id(state_file, tweet.id_str)
+            set_last_id(tweet.id_str)
             logger.info(f'replied to https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}')
         except (Exception, tweepy.TweepError) as error:
             logger.error(error)
